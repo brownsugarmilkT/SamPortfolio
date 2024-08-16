364e10ebd99a33c02801ebf6632e501d0638e54a
Sam <ksamaksh0601@gmail.com>
2024-08-17 00:39:54 +0530

    init cv and projects page

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -29,11 +30,13 @@ export default function Home() {
     
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+
+
   
-        <a
+        <Link
           href="/projects"
           className="group rounded-lg px-5 py-4 transition-all ease-in-out duration-300 transform hover:scale-105 border border-transparent shadow-md hover:border-gray-300 hover:bg-gray-100 hover:shadow-lg hover:dark:border-neutral-700 hover:dark:bg-neutral-800"
-          target="_blank"
+          
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -45,14 +48,14 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             YUH YUH YUH 
           </p>
-        </a>
+        </Link>
 
        
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link
+          href="/cv"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -64,7 +67,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
             One Page ting
           </p>
-        </a>
+        </Link>
       </div>
 
 
